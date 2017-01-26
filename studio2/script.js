c262b98d60f654b7a30b2fd10064ac444ef0d7f4
Andy Lin <andydlin@gmail.com>
2017-01-26 15:12:54 -0800

    Updated google fonts link

--- studio2/script.js
+++ studio2/script.js
@@ -1,4 +1,4 @@
-getGiphy("http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC");
+getGiphy("http://api.giphy.com/v1/gifs/search?q=burger+meme&limit=50&api_key=dc6zaTOxFJmzC");
 
 function getGiphy(url) {
     var xmlHttp = new XMLHttpRequest();
@@ -13,8 +13,14 @@ function getGiphy(url) {
 
 function processGiphy(json) {
     var objects = JSON.parse(json);
-    
-    // for(var i = 0; i < objects.length; i++) {
-    //
-    // }
+    var size = Object.keys(objects['data']).length;
+    var image, imageLink;
+    console.log(objects['data']);
+    for(var i = 0; i < size-1; i++) {
+        // console.log(objects['data'][i]['images']['fixed_width_small']['url']);
+        imageLink = objects['data'][i]['images']['fixed_width']['url'];
+        image = document.createElement('img');
+        image.setAttribute('src', imageLink);
+        document.getElementById('images').appendChild(image);
+    }
 }
